98e37d0d6d853ca31df60fead725f03f9b10be53
mergenj93 <mergenj93@gmail.com>
2022-03-20 03:21:14 +0000

    added vnet file

--- vnet.tf
+++ vnet.tf
@@ -1,14 +1,14 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "StorageAccount-ResourceGroup"
-#     storage_account_name = "team2project"
-#     container_name       = "tfstate"
-#     key                  = "path/to/my/key/prod.terraform.tfstate"
-#     access_key = "pbdzjjYmnpXTUmYIi/bLxl5qhq+iDbkHXCTFe+UhTwi1UoF1ZvzOszr/KcZFXtkvLPgm+YiyX6NI+AStIDDJsA=="
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "team2project"
+    container_name       = "tfstate"
+    key                  = "path/to/my/key/prod.terraform.tfstate"
+    access_key = "pbdzjjYmnpXTUmYIi/bLxl5qhq+iDbkHXCTFe+UhTwi1UoF1ZvzOszr/KcZFXtkvLPgm+YiyX6NI+AStIDDJsA=="
+  }
+}
 
 
 
@@ -80,6 +80,6 @@ output "vnet_id" {
 }
 
 
-output "subnet"{
-    value = azurerm_virtual_network.terraform.subnet
-}
+# output "subnet"{
+#     value = azurerm_virtual_network.terraform.subnet
+# }
